52ab69700512db1fc89c6473e4386152d449096a
Terragor <81566364+zTerragor@users.noreply.github.com>
2023-09-25 01:00:37 +0500

    Kultiras
    Kultiras

--- src/server/scripts/KulTiras/kul_tiras_script_loader.cpp
+++ src/server/scripts/KulTiras/kul_tiras_script_loader.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 ShadowCore
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -15,12 +15,13 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
- // This is where scripts' loading functions should be declared:
-
+// This is where scripts' loading functions should be declared:
+void AddSC_zone_tiragarde_sound();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
+
 void AddKulTirasScripts()
 {
-    
+AddSC_zone_tiragarde_sound();    
 }
